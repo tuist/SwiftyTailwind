bd6b49c000b32e66909e703eb2a436738fb63213
Pedro Piñera <pedro@pepicrft.me>
2023-06-18 15:18:19 +0200

    Add a plugin to generate the documentation

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.5.2")),
-        .package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", .upToNextMinor(from: "1.3.0"))
+        .package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", .upToNextMinor(from: "1.3.0")),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
